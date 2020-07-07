404a076c0213e1b02ba0d054d199ba4f5e825176
Kazuki Wakamatsu <kazuki03black@gmail.com>
2020-07-07 05:30:25 -0700

    Forwardの呼び出しチェック

--- Forward.py
+++ Forward.py
@@ -7,21 +7,19 @@
 class ForwardFunction:
     #float w 　重みとしきい値
     #float e 　入力のデータセット
-    def forward(*w, *e):
-        float u   #入力値
-        float o   #出力値
-        u= 0   #入力値の初期化
-        float threshold    #しきい値
+    def forward(w, e):
+        #float u   #入力値
+        #float o   #出力値
+        o = 100.0
+        u = 0.0   #入力値の初期化
+        #float
+        threshold = 100.0    #しきい値
         for i, j in zip(*w, *e):
             u=u+i*j
             threshold=i    #しきい値はリストwの最後の値
         u=u-threshold     #しきい値の処理
-        o = f(u)
+        o = u              #= f(u)
         return o
 
 
 # In[ ]:
-
-
-
-
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 #これがmain関数
 import getdata
+import Forward
+w = [1,1,1.5]
 a,b = getdata.get_data(4)
 print(a)
 print(b)
+print(w)
+ForwardFunction_class = Forward.ForwardFunction()
+c = ForwardFunction_class.forward(*w,*b)
+
+
+print(c)
